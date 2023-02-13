e576880ab394b9c764534d3eda0bafe535b1340c
kchunz <kokchunzhi@hotmail.com>
2023-02-13 22:30:06 +0800

    Add Remark object under seedu.address.model.person package

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,39 @@
+package seedu.remark.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.remark.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid
+ */
+public class Remark {
+    public final String value;
+
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        this.value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Remark // instanceof handles nulls
+                && value.equals(((Remark) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
